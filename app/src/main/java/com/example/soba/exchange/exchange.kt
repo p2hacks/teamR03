596e65afa6c73d0698841b86b1a804cf26b77f67
kota-shiokara <kota.book.39@gmail.com>
2019-12-07 14:26:40 +0900

    project start

---
+++ app/src/main/java/com/example/soba/exchange/exchange.kt
@@ -0,0 +1,13 @@
+package com.example.soba.exchange
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.soba.R
+
+class exchange : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_exchange)
+    }
+}
---
+++ app/src/main/java/com/example/soba/hide_seek/hide_seek.kt
@@ -0,0 +1,13 @@
+package com.example.soba.hide_seek
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.soba.R
+
+class hide_seek : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hide_seek)
+    }
+}
