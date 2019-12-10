33d17b9d2b72b5de08b722c653b3f3fe9680210a
sora <kacky1210@gmail.com>
2019-12-10 21:15:54 +0900

    navigationで複数の遷移を追加

---
+++ app/src/main/java/com/example/soba/hide_seek/hide_seek_seek_present.kt
@@ -0,0 +1,26 @@
+package com.example.soba.hide_seek
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.example.soba.R
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class BlankFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_blank, container, false)
+    }
+
+
+}
