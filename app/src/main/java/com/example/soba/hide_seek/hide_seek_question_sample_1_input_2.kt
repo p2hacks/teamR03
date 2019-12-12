8dbe35667f31d5abc34a7a5f287b315c1d9e4ce6
yatoto <yamakou12310@icloud.com>
2019-12-12 12:17:58 +0900

    navigation遷移、問題を探す以外終了

--- app/src/main/java/com/example/soba/hide_seek/hide_seek_question_sample_1_input_2.kt
+++ app/src/main/java/com/example/soba/hide_seek/hide_seek_question_sample_1_input_2.kt
@@ -1,7 +1,6 @@
 package com.example.soba.hide_seek
 
-import android.content.Context
-import android.net.Uri
+
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -19,9 +18,6 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Activities that contain this fragment must implement the
- * [hide_seek_question_sample_1_input_2.OnFragmentInteractionListener] interface
- * to handle interaction events.
  * Use the [hide_seek_question_sample_1_input_2.newInstance] factory method to
  * create an instance of this fragment.
  */
@@ -29,7 +25,6 @@ class hide_seek_question_sample_1_input_2 : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
-    private var listener: OnFragmentInteractionListener? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -52,40 +47,6 @@ class hide_seek_question_sample_1_input_2 : Fragment() {
         return view
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    fun onButtonPressed(uri: Uri) {
-        listener?.onFragmentInteraction(uri)
-    }
-
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-        if (context is OnFragmentInteractionListener) {
-            listener = context
-        } else {
-            throw RuntimeException(context.toString() + " must implement OnFragmentInteractionListener")
-        }
-    }
-
-    override fun onDetach() {
-        super.onDetach()
-        listener = null
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     *
-     *
-     * See the Android Training lesson [Communicating with Other Fragments]
-     * (http://developer.android.com/training/basics/fragments/communicating.html)
-     * for more information.
-     */
-    interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        fun onFragmentInteraction(uri: Uri)
-    }
 
     companion object {
         /**
