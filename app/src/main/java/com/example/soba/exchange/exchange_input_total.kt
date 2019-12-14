35a90b60d8fbfe956cbcde2c8a9eb8bc3fb252d1
tianzhongjunping87 <b1019035@fun.ac.jp>
2019-12-14 15:20:03 +0900

    エラーを対処

--- app/src/main/java/com/example/soba/exchange/exchange_input_total.kt
+++ app/src/main/java/com/example/soba/exchange/exchange_input_total.kt
@@ -18,21 +18,25 @@ var totalNumber: Int = 0
 
 class exchange_input_total : Fragment() {
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
 
 
-        ): View? {
+
+    ): View? {
         val view = inflater.inflate(R.layout.fragment_exchange_input_total, container, false)
         view.nextButtonTotal.setOnClickListener {
-            if (inputTotalNumber.text != null) {
-                //　次へボタン を押したときに数字が入力されていれば、その数字を取得
+            //　次へボタン を押したときに数字が入力されていれば、その数字を取得
                 totalNumber = inputTotalNumber.text.toString().toInt()
-            }
-            findNavController().navigate(R.id.action_exchange_input_total_to_exchange_input_name)
-    }
+                findNavController().navigate(R.id.action_exchange_input_total_to_exchange_input_name)
+        }
         return view
     }
 }
