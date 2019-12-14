b0e868beb7c447e99130001ad462bb3a5df6445c
kota-shiokara <kota.book.39@gmail.com>
2019-12-14 17:10:20 +0900

    一連の流れ(開始〜終了、メインに遷移)を実装完了

--- app/src/main/java/com/example/soba/exchange/exchange_explanation.kt
+++ app/src/main/java/com/example/soba/exchange/exchange_explanation.kt
@@ -9,34 +9,22 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import com.example.soba.R
+import kotlinx.android.synthetic.main.fragment_exchange_explanation.view.*
 import kotlinx.android.synthetic.main.fragment_exchange_play.view.*
 
-
-/**
- * A simple [Fragment] subclass.
- * Activities that contain this fragment must implement the
- * [exchange_explanation.OnFragmentInteractionListener] interface
- * to handle interaction events.
- * Use the [exchange_explanation.newInstance] factory method to
- * create an instance of this fragment.
- *
- */
 class exchange_explanation : Fragment() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 
+
+        // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_exchange_explanation, container, false)
-        view.nextButtonPreGeter.setOnClickListener {
+        view.startButton.setOnClickListener {
             findNavController().navigate(R.id.action_exchange_explanation_to_exchange_play)
         }
-        // Inflate the layout for this fragment
         return view
     }
 }
