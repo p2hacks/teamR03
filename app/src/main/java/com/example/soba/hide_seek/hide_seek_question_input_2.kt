c3799184f4d8ef89cce7f27e75d1d339e8f674c1
sora <kacky1210@gmail.com>
2019-12-14 17:06:13 +0900

    問題文の保存、シャッフルして表示を実装

--- app/src/main/java/com/example/soba/hide_seek/hide_seek_question_input_2.kt
+++ app/src/main/java/com/example/soba/hide_seek/hide_seek_question_input_2.kt
@@ -1,38 +1,23 @@
 package com.example.soba.hide_seek
 
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.edit
 import androidx.navigation.fragment.findNavController
 
 import com.example.soba.R
 import kotlinx.android.synthetic.main.fragment_hide_seek_question_input_2.view.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
 /**
  * A simple [Fragment] subclass.
- * Use the [hide_seek_question_input_2.newInstance] factory method to
- * create an instance of this fragment.
- */
-class hide_seek_question_input_2 : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+ * */
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+class hide_seek_question_input_2 : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -43,24 +28,24 @@ class hide_seek_question_input_2 : Fragment() {
         return view
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment hide_seek_question_input_2.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            hide_seek_question_input_2().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+        view.question2_save.setOnClickListener {
+            //ボタン押下時テキストボックスの内容の受け取りと表示
+            val question2 = view.edit_question2.text.toString()
+            val choice1 = view.input2_choice1.text.toString()
+            val choice2 = view.input2_choice2.text.toString()
+            val choice3 = view.input2_choice3.text.toString()
+
+            //SharePreferencesのquestionSet2にキーを↑で取得した内容を保存
+            activity?.getSharedPreferences("questionSet2", Context.MODE_PRIVATE)?.edit {
+                putString("question", question2)
+                putString("choice1", choice1)
+                putString("choice2", choice2)
+                putString("choice3", choice3)
+                apply()
             }
+        }
     }
 }
